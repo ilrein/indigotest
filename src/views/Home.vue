784742c03e164c02aeb313a6afb09ee8ed645f87
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2020-09-18 13:47:26 -0400

    container layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,30 +7,32 @@
         Indigo Test
       </sui-header>
 
-      <sui-card
-        v-for="product in products"
-        :key="product.id"
-      >
-        <sui-image
-          :src="product.image"
-        />
-        <sui-card-content>
-          <sui-card-header>
-            {{ product.name }}
-          </sui-card-header>
-          <sui-card-description>
-            {{ product.description }}
-          </sui-card-description>
-        </sui-card-content>
-        <sui-card-content extra>
-          <router-link
-            :to="`product/${product.id}`"
-          >
-            <sui-icon name="shopping cart" />
-            Buy now!
-          </router-link>
-        </sui-card-content>
-      </sui-card>
+      <sui-card-group :items-per-row="3">
+        <sui-card
+          v-for="product in products"
+          :key="product.id"
+        >
+          <sui-image
+            :src="product.image"
+          />
+          <sui-card-content>
+            <sui-card-header>
+              {{ product.name }}
+            </sui-card-header>
+            <sui-card-description>
+              {{ product.description }}
+            </sui-card-description>
+          </sui-card-content>
+          <sui-card-content extra>
+            <router-link
+              :to="`product/${product.id}`"
+            >
+              <sui-icon name="shopping cart" />
+              Buy now!
+            </router-link>
+          </sui-card-content>
+        </sui-card>
+      </sui-card-group>
     </sui-container>
   </div>
 </template>
