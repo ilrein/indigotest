f14b630bc6b2f9a940d827a7f90cb2c97a37eea0
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2020-09-18 13:32:27 -0400

    basics

---
+++ src/views/Buy.vue
@@ -0,0 +1,10 @@
+<template>
+  <sui-container>
+    <h1>
+      Buy now page!
+    </h1>
+    <h2>
+      Sorry, this product data is randomly generated and has no basis in reality :(
+    </h2>
+  </sui-container>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sui-container>
+      <sui-header
+        style="font-size: 50px; margin-top: 1rem;"
+      >
+        Indigo Test
+      </sui-header>
+
+      <sui-card
+        v-for="product in products"
+        :key="product.id"
+      >
+        <sui-image
+          :src="product.image"
+        />
+        <sui-card-content>
+          <sui-card-header>
+            {{ product.name }}
+          </sui-card-header>
+          <sui-card-description>
+            {{ product.description }}
+          </sui-card-description>
+        </sui-card-content>
+        <sui-card-content extra>
+          <router-link
+            :to="`product/${product.id}`"
+          >
+            <sui-icon name="shopping cart" />
+            Buy now!
+          </router-link>
+        </sui-card-content>
+      </sui-card>
+    </sui-container>
   </div>
 </template>
 
 <script>
+import fetch from 'isomorphic-fetch';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+  },
+  async mounted() {
+    const get = await fetch('http://localhost:3000/api/products');
+
+    const result = await get.json();
+
+    this.products = result.products;
+    console.log(this.products);
+  },
+  data() {
+    return {
+      products: [],
+    };
   },
 };
 </script>
